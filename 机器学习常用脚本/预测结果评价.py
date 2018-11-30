371c90039ecd4e4357c99401e3711441083db0e8
luanshiyinyang <1695735420@qq.com>
2018-11-30 18:22:47 +0800

    脚本

---
+++ 机器学习常用脚本/预测结果评价.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc
+# X表示真实结果标签
+X = np.array([1, 2, 2, 1, 2, 1, 0])
+# Y表示预测结果标签
+Y = np.array([2, 1, 2, 1, 2, 1, 0])
+# 设定积极标签个数
+fpr, tpr, thresholds = metrics.roc_curve(X, Y, pos_label=1)
+
+# 绘制ROC曲线
+plt.plot(fpr, tpr, marker='o')
+plt.show()
+
+# 求出AUC值
+AUC = auc(fpr, tpr)
+print(AUC)
+
+# 得到混淆矩阵
+# labels表示所有的标签种类个数
+mess = metrics.confusion_matrix(X, Y, labels=[0, 1, 2])
+print(mess)
